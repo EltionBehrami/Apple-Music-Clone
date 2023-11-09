b7c26a0478aa1e6cb15f933d2b4cb0bf877f1406
“Eltion <ebehrami01@gmail.com>
2023-11-09 15:09:41 -0500

    user model complete and seeded

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+    has_secure_password 
+    
+    before_validation :ensure_session_token
+    validates :email, :session_token, presence: true, uniqueness: true 
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { in: 3..255 }
+    validates :name, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    
+
+    def self.find_by_credentials(email, password) 
+        user = User.find_by(email: email) 
+        if user && user.authenticate(password)
+            user 
+        else 
+            nil
+        end 
+    end 
+
+    def reset_session_token! 
+        self.session_token = generate_session_token 
+        self.save! 
+        self.session_token
+    end 
+
+
+    private 
+    
+    def generate_session_token 
+        loop do 
+            session_token = SecureRandom.base64
+            break session_token unless User.exists?(session_token: session_token)
+        end 
+    end 
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end 
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    User.destroy_all 
+
+    puts "Resetting primary keys..."
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+
+    puts "Creating users..."
+
+    User.create!(
+    name: 'Demo-User', 
+    email: 'demo@user.io', 
+    password: 'password',
+    birthdate: Faker::Date.between(from: '2000-09-23', to: '2014-09-25')
+    )
+
+    10.times do 
+        User.create!({
+            name: Faker::Name.name,
+            email: Faker::Internet.unique.email,
+            password: 'password',
+            birthdate: Faker::Date.between(from: '2000-09-23', to: '2014-09-25')
+        }) 
+    end
+
+
+    puts "Done!"
+
+end 
