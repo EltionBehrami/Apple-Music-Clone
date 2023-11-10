79cce0a11a8d5bd01d9de643c310f610735b4333
“Eltion <ebehrami01@gmail.com>
2023-11-09 19:22:36 -0500

    backend auth completed and tested

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,2 +1,3 @@
-
-json.user
+json.user do 
+    json.extract! @user, :id, :email, :name, :created_at, :updated_at
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,13 @@ ApplicationRecord.transaction do
     name: 'Demo-User', 
     email: 'demo@user.io', 
     password: 'password',
-    birthdate: Faker::Date.between(from: '2000-09-23', to: '2014-09-25')
     )
 
     10.times do 
         User.create!({
             name: Faker::Name.name,
             email: Faker::Internet.unique.email,
-            password: 'password',
-            birthdate: Faker::Date.between(from: '2000-09-23', to: '2014-09-25')
+            password: 'password'
         }) 
     end
 
