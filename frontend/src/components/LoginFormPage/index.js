a6105d12b1ec69e5d1810db07ba667a0d9609be7
“Eltion <ebehrami01@gmail.com>
2023-11-10 10:12:05 -0500

    login and signup components mounted on page

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../store/session";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState([])
+
+    if (sessionUser) return <Redirect to="/" />;
+
+    const handleSubmit = e => {
+        let user = { email, password }
+        return dispatch(login(user))
+            .catch(async (res) => {
+                let data;
+                try {
+                    data = await res.clone.json(); 
+                } catch {
+                    data = await res.text(); 
+                }
+                if (data?.errors) setErrors(data.errors)
+                else if (data) setErrors([data]);
+                else setErrors([res.statusText])
+            })
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <ul>
+                {errors.map(error => <li key={error}>{error}</li>)}
+            </ul>
+            <label>Email
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </label>
+            <label>Password
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    )
+
+
+}
+
+
+export default LoginFormPage; 
