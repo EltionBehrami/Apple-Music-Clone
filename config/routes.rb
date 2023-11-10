a58f1b13b4aaff1c7536ecf0dbf09508df52e71a
“Eltion <ebehrami01@gmail.com>
2023-11-09 20:00:04 -0500

    csrf fetch requests implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
-  post 'api/test', to: 'application#test'
-
   namespace :api, defaults: { format: :json } do 
     resources :users, only: :create 
     resource :session, only: [:create, :destroy, :show]
