7ac8817b03e931f68bdd015b6ac857d0745b75da
“Eltion <ebehrami01@gmail.com>
2023-11-10 15:57:04 -0500

    styling login form

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,15 +1,33 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import ProfileButton from "./ProfileButton";
+import { useSelector } from "react-redux";
 
 const Navigation = () => {
     
+    const sessionUser = useSelector(state => state.session.user)
+
+    let sessionLinks; 
+    if (sessionUser) {
+        sessionLinks = (<ProfileButton user={sessionUser} />)
+    } else {
+        sessionLinks = (
+            <>
+                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/signup">Sign Up</NavLink>
+            </>
+        );
+    } 
+
+    
+
+
     return (
         <ul>
+            <li>
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/signup">Sign Up</NavLink>
-            <ProfileButton />
-        </ul>
+            {sessionLinks}
+            </li>
+        </ul>   
     )
 }
 
